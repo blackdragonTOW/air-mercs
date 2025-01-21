383b292dd64d7f5f631cba4fd1fba16b2471ac23
blackdragonTOW <31581761+blackdragonTOW@users.noreply.github.com>
2025-01-20 21:08:09 -0800

    Case statements handlebars helpers

--- module/air-mercs.mjs
+++ module/air-mercs.mjs
@@ -87,6 +87,21 @@ Handlebars.registerHelper('toLowerCase', function (str) {
   return str.toLowerCase();
 });
 
+Handlebars.registerHelper('switch', function(value, options) {
+  this.switch_value = value;
+  return options.fn(this);
+});
+
+Handlebars.registerHelper('case', function(value, options) {
+  if (value == this.switch_value) {
+    return options.fn(this);
+  }
+});
+
+Handlebars.registerHelper('default', function(value, options) {
+    return true; 
+});
+
 /* -------------------------------------------- */
 /*  Ready Hook                                  */
 /* -------------------------------------------- */
